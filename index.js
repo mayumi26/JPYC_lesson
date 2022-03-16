d77d4a2e12c393ce8d63fe81a4f5be5607f739e1
ponchan <100114313+ponchan26@users.noreply.github.com>
2022-03-16 09:55:07 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,4 @@
-/* console.log("hellow world");
-console.log("こんにちは");
-console.log("gm");
 
-const provider = new ethers.providers.Web3Provider(window.ethereum)
-
-provider.send("eth_requestAccounts", []);
-
-const signer = provider.getSigner()
-
- a = provider.getBlockNumber();
- console.log(a); */
-
-
-/* window.onload = function() {
- alert("ページが読み込まれました！");
-}; */
-
- 
 window.onload = function() {
  console.log("ページを読み込みました！");
 };
