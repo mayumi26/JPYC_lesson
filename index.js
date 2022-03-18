225d2d0949c4b950693da7bc6cf225169da16f0e
ponchan <100114313+ponchan26@users.noreply.github.com>
2022-03-18 10:41:21 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-const provider = new ethers.providers.Web3Provider(window.ethereum)
-await provider.send("eth_requestAccounts", []);
-const signer = provider.getSigner()
-
-let a;
-window.onload = async function myfunc1(){
-  a = ethers.getSinger()
+console.log("hello");
